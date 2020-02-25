6b385c92683957ae5c7483c3f2b88b2838dd7e59
Wei Lu <wadelu0714@gmail.com>
2020-02-25 16:16:19 +1300

    strat the project and set the navigation

---
+++ screens/CategoryMealsScreen.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { View, Text, Button, StyleSheet } from 'react-native'
+
+const CategoryMealsScreen = props => {
+  return (
+    <View style={styles.screen}>
+      <Text>The Category Meals Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => {
+          props.navigation.navigate({
+            routeName: 'MealDetail'
+          })
+        }}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
+
+export default CategoryMealsScreen
